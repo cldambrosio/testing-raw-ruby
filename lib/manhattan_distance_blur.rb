35452b367c1fa830ad4056738684ea05d183e65e
Claire D'Ambrosio <claire.dambrosio@gmail.com>
2017-08-14 10:42:15 +0000

    implementing a blurring solution for specified distance

---
+++ lib/manhattan_distance_blur.rb
@@ -0,0 +1,55 @@
+class Image
+  attr_accessor :image
+
+  def initialize(image)
+    @image = image
+  end
+
+  def output_image
+    @image.each { |row| puts row.join }
+  end
+
+  def manhattan_distance_blur(distance, image=nil)
+    image ||= @image
+
+    blurred_image = Array.new(image.length) { Array.new(image[0].length, 0) }
+   
+    image.each_with_index do |row, i|
+      row.each_with_index do |pixel, j|
+        if pixel == 1
+          blurred_image[i][j] = 1                             # SAME ROW AND COLUMN
+          if j >= distance
+            blurred_image[i][j-1] = 1                         # LEFT
+          end
+          if i >= distance
+            blurred_image[i-1][j] = 1                         # TOP
+          end
+          if row.length > (j+1)
+            blurred_image[i][j+1] = 1                         # RIGHT
+          end
+          if image.length > (i+distance)
+            blurred_image[i+1][j] = 1                         # BOTTOM
+          end
+        end
+      end
+    end
+
+    if distance > 1
+      manhattan_distance_blur(distance-1, blurred_image)
+    else
+      blurred_image.each { |row| puts row.join }
+    end
+  end
+end
+
+
+# image = Image.new([
+# [1,0,0,0,0,0,0,0],
+# [0,0,0,0,0,0,0,0],
+# [0,0,0,0,0,0,0,0],
+# [0,0,0,0,0,1,0,0],
+# [0,0,0,0,0,0,0,0],
+# [0,0,0,0,0,0,0,0]
+# ])
+
+# image.manhattan_distance_blur(3)
--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe Image, type: :model do
      expect(image.blur).to eq expected
     end
   end
+
+  describe "specified manhattan distance blurring should work" do
+    it "should blur a standard image" do
+      input = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ]
+      expected = [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [1, 1, 1, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0]
+      ]
+      image = Image.new(input)
+      expect(image.manhattan_distance_blur(2)).to eq expected
+    end
+  end
+
 end
